b57489be7d6b3b9a42ebfb13b9a1fb7527592f89
Theieyrre <crizftfm@gmail.com>
2022-01-19 09:07:46 +0300

    Update view and page structure

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -6,6 +6,8 @@ class Login extends BaseController
 {
     public function index()
     {
+        echo view('templates/header');
         return view('login');
+        echo view('templates/footer');
     }
 }
--- app/Views/login.php
+++ app/Views/login.php
@@ -1,44 +1,39 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-
-    <title>TODO Uygulamasına giriş yapın!</title>
-  </head>
-  <body>
     <div class="container">
         <div class="row d-flex justify-content-center">
             <h2 class="title">CodeIgniter TODO Uygulaması</h2>
         </div>
         <div class="row d-flex justify-content-center">
-            <form>
+            <form method="post">
                 <div class="form-group">
                     <label for="email">Email adresiniz</label>
-                    <input type="email" class="form-control" id="email">
+                    <input type="email" class="form-control" id="email" name="email">
                 </div>
                 <div class="form-group">
                     <label for="password">Şifreniz</label>
-                    <input type="password" class="form-control" id="password">
+                    <input type="password" class="form-control" id="password" name="password">
                 </div>
                 <div class="d-flex justify-content-center">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary" name="login">Submit</button>
                 </div>
                 
             </form>
         </div>
     </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <?php 
+
+        if(isset($_POST["login"])){
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $query = $this->db->query('SELECT user_id FROM users WHERE email='.$this->db->escape($email).' AND password='.$password.'');
+            $row = $query->row();
+
+            if(isset($row)){
+
+            }
+        }
+    
+    ?>
+
+    
